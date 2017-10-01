a5441a8259e902c18f804e19e0c1947145d2e78c
Joey Miller <miller_joseph3@bah.com>
2017-10-01 17:59:18 -0400

    Did a lot of reading about how to serve static files instead of hosting a bunch of random endpoints for assets.

--- app.js
+++ app.js
@@ -2,43 +2,18 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-var directory;
+loadPage('/', 'pages/index.html');
 
-if (process.env.ENVIRONMENT === 'dev') {
-    directory = '/home/ec2-user'
-} else {
-    directory = '/users/josephmiller'
-}
+loadPage('/main', 'pages/main.html');
 
-app.get('/',function(req,res){
-    res.sendFile(directory + '/Projects/FPSite/pages/login.html');
-    //__dirname : It will resolve to your project folder.
-});
-
-app.get('/main',function(req,res){
-    res.sendFile(directory + '/Projects/FPSite/pages/main.html')
-});
-
-app.get('/bulma.css',function(req,res) {
-    res.sendFile(directory + '/Projects/FPSite/css/bulma.css')
-});
-
-app.get('/images/fpShort',function(req,res){
-    res.sendFile(directory + '/Projects/FPSite/images/fpShort.png');
-});
-
-app.get('/images/fpLong',function(req,res){
-    res.sendFile(directory + '/Projects/FPSite/images/fpLong.png');
-});
-
-app.get('/images/fb',function(req,res){
-    res.sendFile(directory + '/Projects/FPSite/images/fb.png');
-});
-
-app.get('/images/discord',function(req,res){
-    res.sendFile(directory + '/Projects/FPSite/images/discord.svg');
-});
+app.use('/', express.static('assets'));
 
 app.listen(3000);
 
 console.log("Running at Port 3000");
+
+function loadPage(directory, fileName) {
+    app.get(directory, function(req,res){
+        res.sendFile(path.resolve(__dirname, fileName))
+    });
+}
