139148abb3413e8259f1b6d72ec267da363bad17
Joey Miller <miller_joseph3@bah.com>
2017-10-02 11:20:12 -0400

    Changed method for /main page to be post to accept a submit button

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-loadPage('/', 'pages/index.html');
+app.get('/', function(req,res){
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+});
 
-loadPage('/main', 'pages/main.html');
+app.post('/main', function(req,res){
+    res.sendFile(path.resolve(__dirname, 'pages/main.html'))
+});
 
 app.use('/', express.static('assets'));
 
 app.listen(3000);
 
 console.log("Running at Port 3000");
-
-function loadPage(directory, fileName) {
-    app.get(directory, function(req,res){
-        res.sendFile(path.resolve(__dirname, fileName))
-    });
-}
